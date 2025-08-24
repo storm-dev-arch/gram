c1a97d43414d4c8cd795068ce5f4de6e1c98c410
storm-dev-arch <zifi3639@gmail.com>
2025-08-24 19:32:12 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
+@app.route('/<path:path>')
+def static_files(path):
+    return send_from_directory('static', path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
